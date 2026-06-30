e660c73418a81790234d2861f3bd54da4f7fb612
akashroy7564 <hiakashkumar123@gmail.com>
2026-06-30 14:24:33 +0530

    error solving

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://quickcart-vr1o.onrender.com/api/api",
+    baseURL: "https://quickcart-vr1o.onrender.com/api",
 });
 
 // Automatically attach JWT token
